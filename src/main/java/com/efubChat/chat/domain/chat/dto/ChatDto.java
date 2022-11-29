22beeecacbe9497de26d18ece51a1e9195d1a939
SR <lsr_1105@ewhain.net>
2022-11-30 00:50:49 +0900

    fix: fix chat room return factors

--- src/main/java/com/efubChat/chat/domain/chat/dto/ChatDto.java
+++ src/main/java/com/efubChat/chat/domain/chat/dto/ChatDto.java
@@ -1,5 +1,6 @@
 package com.efubChat.chat.domain.chat.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import lombok.*;
@@ -16,10 +17,12 @@ public class ChatDto {
     private Long id;
     private Long chatRoomId;
     private UUID memberId;
+    private String state;
     private String nickname;
     private String message;
     private String region;
 
     @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime registerDate;
 }
